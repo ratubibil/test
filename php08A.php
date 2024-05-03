46234d8c30b478eba1570422d66285b7b4129957
ratubibil <168807568+ratubibil@users.noreply.github.com>
2024-05-03 13:38:00 +0700

    Add files via upload

---
+++ php08A.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html> <html lang=’en-GB’>
+    <head>
+        <title>Hello World</title>
+        <meta name="author" content="Ratu Bilqis Azzuhrah">
+        <meta name="copyright" content="Ratu Bilqis Azzuhrah">
+        <meta name="creation-date" content="2024-05-03">
+    </head>
+    <body>
+        <h1>Our first PHP script</h1>
+<?php
+$user = "Ratu Bilqis Azzuhrah";
+print ("<p><b>Hello $user<br>\nHello World!</b></p>\n"); ?>
+    </body>
+</html>
